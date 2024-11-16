f1814e648da8a341f1bd33f88f1f494da0541e75
Vladislav Ostanin <ostanin8900@yandex.ru>
2024-11-17 01:53:29 +0500

    создал конфиг сервер и докер файлы для 2 сервисов. + Докер композ

--- configserver/src/main/java/com/optimagrowth/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/optimagrowth/configserver/ConfigserverApplication.java
@@ -1,13 +1,13 @@
-package com.optimagrowth.eurekaserver;
+package com.optimagrowth.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EurekaserverApplication {
+public class ConfigserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
+		SpringApplication.run(ConfigserverApplication.class, args);
 	}
 
 }
--- configserver/src/test/java/com/optimagrowth/configserver/ConfigserverApplicationTests.java
+++ configserver/src/test/java/com/optimagrowth/configserver/ConfigserverApplicationTests.java
@@ -1,10 +1,10 @@
-package com.optimagrowth.eurekaserver;
+package com.optimagrowth.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaserverApplicationTests {
+class ConfigserverApplicationTests {
 
 	@Test
 	void contextLoads() {
