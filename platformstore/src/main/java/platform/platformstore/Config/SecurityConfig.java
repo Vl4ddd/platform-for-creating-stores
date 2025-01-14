8cdbd0877e706ef4c2b6257780cdb963828751b8
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-01-14 10:55:18 +0500

    add securityfilter chain

--- platformstore/src/main/java/platform/platformstore/Config/SecurityConfig.java
+++ platformstore/src/main/java/platform/platformstore/Config/SecurityConfig.java
@@ -1,5 +1,42 @@
 package platform.platformstore.Config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth**").permitAll()
+                        .requestMatchers("/api/v1/crud/**").authenticated()
+                        .requestMatchers("/swagger-ui/**", "/swagger-resources/**", "/api/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .logout(logout -> logout
+                        .logoutUrl("/api/v1/auth/logout")
+                        .invalidateHttpSession(true) // Уничтожить сессию
+                        .deleteCookies("JSESSIONID") // Удалить куки сессии
+                        .permitAll()
+                        .logoutSuccessUrl("/api/v1/auth/login"))
+                
+        return http.build();
+    }
 }
